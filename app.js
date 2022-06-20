337061d35faa6faa8e5e7dd6e6f41d6e26723976
yejin <jynhpss@naver.com>
2022-06-20 22:35:35 +0900

    브러쉬 색상변경 기능

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.querySelector("#jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.querySelectorAll(".jsColor");
 
 // canvas에 css로 width, height 값을 주는 것 뿐만 아니라 
 // 픽셀을 다루는 윈도우가 얼마나 큰지 canvas에 픽셀 width, height를 줘야한다.
@@ -19,25 +20,21 @@ function onMouseMove(event){
 
   // 마우스를 클릭하지 않아서 painting이 false일 때 캔버스 위에서 움직이면서 좌표값만 받아온다.
   // 마우스를 down하게 되면 painting이 true가 되면서 false일때 최근 path값을 시작으로 마우스를 up할때까지 선을 긋는다.
-  if(!painting){
-    // beginPath 마우스를 클릭하지 않고 있을 때 마우스가 위치한 곳에 path를 시작한다는 것이다. path = 선
-    ctx.beginPath();
-    // moveTo 마우스가 위치한 곳의 x, y 좌표값을 가져온다.
-    ctx.moveTo(x, y);
-  }else{
+  if(painting === true){
     // lineTo는 전 위치에서 지금 위치까지 선을 만드는 메소드다.
     ctx.lineTo(x, y);
     // stroke는 현재의 sub-path를 현재의 stroke style로 획을 그은다.
     ctx.stroke();
+  }else{
+    // beginPath 마우스를 클릭하지 않고 있을 때 마우스가 위치한 곳에 path를 시작한다는 것이다. path = 선
+    ctx.beginPath();
+    // moveTo 마우스가 위치한 곳의 x, y 좌표값을 가져온다.
+    ctx.moveTo(x, y);
   }
 }
 
 function onMouseDown(event){
   painting = true;
-}
-
-function onMouseUp(event){
-  stopPainting();
 }
 
 function startPainting(){
@@ -54,3 +51,18 @@ if(canvas){
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
+
+
+function handleColorClick(event){
+  const color = event.target.style.backgroundColor
+  ctx.strokeStyle = color;
+}
+
+// Array.from() 은 object로부터 array를 만든다
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
+
+
+/* canvas는 html의 한 요소인데 다른 점은 context를 갖는다는거야. 
+context는 이 요소 안에서 우리가 픽셀에 접근할 수 있는 방법이야. 
+이론적으로는 이 안에 있는 픽셀들인거지 
+getContext()를 만들면 사용할 수 있다. */
